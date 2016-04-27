66999543e110a219498a1fef702cf48c8e291154
Felix <felix.yeung@icloud.com>
2016-04-27 22:49:27 +0200

    Nur window

--- src/main/java/ch/fhnw/project/App.java
+++ src/main/java/ch/fhnw/project/App.java
@@ -12,33 +12,13 @@ public final class App extends Application {
 
     @Override
     public void start(Stage stage) {
-//
-      /*  //test commit
-        //testtst
-        // Asserts correct project setup (will only compile with Java 8)
-        Supplier<String> helloWorldSupplier = () -> "Hello World";
-        System.out.println(helloWorldSupplier.get());*/
-        Button button = new Button("click me...");
-
-        button.setOnAction(event -> {
-            System.out.println("hey :D");
-            button.setStyle("-fx-background-color: blue");
-
-        });
-
-// was isch los?!
-
-        //Button button2 =new Button("Button2");
-
 
 
         Pane pane = new Pane();
-        pane.getChildren().add(button);
-
-        Scene scence = new Scene(pane,300,200);
+               Scene scence = new Scene(pane,300,200);
 
 
-        stage.setTitle("hellllouuuuuu");
+        stage.setTitle("App_Project");
         stage.setScene(scence);
         stage.show();
 
