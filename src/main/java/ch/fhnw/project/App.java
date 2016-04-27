a0c0520196c70a9bb166a564c64d6eb914174e16
Felix <felix.yeung@icloud.com>
2016-04-27 12:48:09 +0200

    this is a test

--- src/main/java/ch/fhnw/project/App.java
+++ src/main/java/ch/fhnw/project/App.java
@@ -9,6 +9,9 @@ public final class App extends Application {
 
     @Override
     public void start(Stage stage) {
+
+        //test commit
+        //testtst
         // Asserts correct project setup (will only compile with Java 8)
         Supplier<String> helloWorldSupplier = () -> "Hello World";
         System.out.println(helloWorldSupplier.get());
